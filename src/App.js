d1fd066dd43545146a345d0f182621d1fcad709c
Igo Quintino <igocastro.15@gmail.com>
2023-11-09 19:25:33 -0300

    funcionando os testes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,80 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
-  const [repositories, setRepositories] = useState( [] );
+  const [repositories, setRepositories] = useState([]);
+  // const [showRepositories, setShowRepositories] = useState(false);
 
-  useEffect( () => {
-    api.get('/repositories').then( response =>
-      setRepositories( response.data )
+
+  useEffect(() => {
+    api.get('/repositories').then(response =>
+      setRepositories(response.data)
     );
-  }, [] );
+  }, []);
 
   async function handleAddRepository() {
-    // TODO
-     const response = await api.post('/repositories', {
-      title: 'Prog Web',
-      url: 'ufopa.edu.br',
-      techs: ['Python', 'Node.JS', 'ReactJS'],
-    })
+    try {
+      const response = await api.post('/repositories', {
+        title: 'Prog Web',
+        url: 'ufopa.edu.br',
+        techs: ['Python', 'Node.JS', 'ReactJS'],
+      });
 
-    setRepositories( [...repositories, response.data ] );
-
+      setRepositories([...repositories, response.data]);
+      console.log('Repositório adicionado com sucesso!');
+    } catch (error) {
+      console.error('Erro ao adicionar repositório:', error);
+    }
   }
+
+  async function handleListRepositoriesByTitle(title) {
+    const response = await api.get('/repositories');
+
+    const filteredRepositories = response.data.filter(repo => repo.title === title);
+
+    setRepositories(filteredRepositories);
+  }
+
+  //  function handleButtonClick() {
+  //   setShowRepositories(!showRepositories);
+  //  }
+
+
+
 
   async function handleRemoveRepository(id) {
-    // TODO
+    // Remove o repositório do estado local
+    const newRepositories = repositories.filter(repository => repository.id !== id);
+    setRepositories(newRepositories);
+
+    //Envia uma solicitação DELETE para a API para remover o repositório
+    await api.delete(`/repositories/${id}`);
   }
+
 
   return (
     <div>
       <ul data-testid="repository-list">
-        {repositories.map( repository => (
-          <li key = {repository.id}>
-          {repository.title}
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
         ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
+      <br />
+      <button onClick={handleListRepositoriesByTitle}>Listar</button>
+      {/* <br/>
+      <button onClick={handleButtonClick}>Mostrar repositórios</button> */}
+
     </div>
   );
 }
